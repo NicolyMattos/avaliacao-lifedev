9d5569890fd623f53b2fe904f61ea225c429f239
NicolyMattos <nicoly.souza4@fatec.sp.gov.br>
2025-05-10 02:47:50 -0300

    implementacao-config

--- src/firebase/config.jsx
+++ src/firebase/config.jsx
@@ -1,20 +1,18 @@
 import { initializeApp } from "firebase/app"
-import { getAnalytics } from "firebase/analytics"
 import { getFirestore } from "firebase/firestore"
 import { getAuth } from "firebase/auth"
 
 const firebaseConfig = {
-    apiKey: "",
-    authDomain: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: "",
-    measurementId: ""
+    apiKey: "AIzaSyCaIQZN6sl2q2uY08CLdi97e5Nd2xbuT-U",
+    authDomain: "avaliacao-dw3-lifedev.firebaseapp.com",
+    projectId: "avaliacao-dw3-lifedev",
+    storageBucket: "avaliacao-dw3-lifedev.firebasestorage.app",
+    messagingSenderId: "255978404968",
+    appId: "1:255978404968:web:7dcb8e83d7470277384fdd",
+    measurementId: "G-6NBXDSPG7L"
 };
 
 const app = initializeApp(firebaseConfig)
-const analytics = getAnalytics(app)
 const db = getFirestore(app)
 const auth = getAuth(app)
 
